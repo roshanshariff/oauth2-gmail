e61796c45d6b35a53b691d330ec0004ec287a230
Roshan Shariff <roshan.shariff@gmail.com>
2021-04-03 00:16:40 -0600

    Minor refactoring

--- oauth2_gmail/main.py
+++ oauth2_gmail/main.py
@@ -13,33 +13,27 @@ import typer
 app = typer.Typer()
 
 
-def save_credentials(name, credentials: Credentials):
+def save_credentials(name: str, credentials: Credentials):
     keyring.set_password("oauth2-gmail", name, credentials.to_json())
+
+
+def load_credentials(name: str) -> dict[str, str]:
+    credentials_json = keyring.get_password("oauth2-gmail", name)
+    if not credentials_json:
+        return dict()
+    return json.loads(credentials_json)
 
 
 @app.command()
 def authorize(
-        name: str = typer.Option(
-            "default",
-            help="Name for this credential."
-        ),
-        client_secrets: Path = typer.Argument(
-            ...,
-            help="Path to client secrets JSON file.",
-            exists=True,
-            readable=True,
-            dir_okay=False
-        ),
-        headless: bool = typer.Option(
-            False,
-            help="Run a console-based flow."
-        )
+        name: str = typer.Option("default", help="Name for this credential."),
+        client_secrets: Path = typer.Argument(..., help="Path to client secrets JSON file.",
+                                              exists=True, readable=True, dir_okay=False),
+        headless: bool = typer.Option(False, help="Run a console-based flow.")
 ):
 
-    flow = InstalledAppFlow.from_client_secrets_file(
-        client_secrets,
-        scopes=['https://mail.google.com/']
-    )
+    flow = InstalledAppFlow.from_client_secrets_file(client_secrets,
+                                                     scopes=['https://mail.google.com/'])
 
     credentials = flow.run_console() if headless else flow.run_local_server()
     save_credentials(name, credentials)
@@ -47,20 +41,13 @@ def authorize(
 
 @app.command()
 def get(
-        name: str = typer.Argument(
-            "default",
-            help="Name for this credential."
-        ),
-        force_refresh: bool = typer.Option(
-            False,
-            help="Force-refresh token."
-        )
+        name: str = typer.Argument("default", help="Name for this credential."),
+        force_refresh: bool = typer.Option(False, help="Force-refresh token.")
 ):
 
-    credentials_json = keyring.get_password("oauth2-gmail", name)
-    if not credentials_json:
+    credentials_dict = load_credentials(name)
+    if not {'client_id', 'client_secret', 'refresh_token'}.issubset(credentials_dict):
         raise typer.Exit(code=1)
-    credentials_dict = json.loads(credentials_json)
 
     token = credentials_dict.get('token')
 
