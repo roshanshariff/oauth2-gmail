9e9ee878c319dae2f7af61da51796855d89f263b
Roshan Shariff <roshan.shariff@gmail.com>
2021-04-03 18:02:55 -0600

    Add check command and fix mypy type annotations

--- oauth2_gmail/main.py
+++ oauth2_gmail/main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from datetime import datetime, timezone, timedelta
 import dateutil.parser
 import json
+from typing import Optional, cast
 
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -13,7 +14,7 @@ import typer
 app = typer.Typer()
 
 
-def save_credentials(name: str, credentials: Credentials):
+def save_credentials(name: str, credentials: Credentials) -> None:
     keyring.set_password("oauth2-gmail", name, credentials.to_json())
 
 
@@ -21,33 +22,15 @@ def load_credentials(name: str) -> dict[str, str]:
     credentials_json = keyring.get_password("oauth2-gmail", name)
     if not credentials_json:
         return dict()
-    return json.loads(credentials_json)
+    credentials_dict = json.loads(credentials_json)
+    assert isinstance(credentials_dict, dict)
+    return credentials_dict
 
-
-@app.command()
-def authorize(
-        name: str = typer.Option("default", help="Name for this credential."),
-        client_secrets: Path = typer.Argument(..., help="Path to client secrets JSON file.",
-                                              exists=True, readable=True, dir_okay=False),
-        headless: bool = typer.Option(False, help="Run a console-based flow.")
-):
-
-    flow = InstalledAppFlow.from_client_secrets_file(client_secrets,
-                                                     scopes=['https://mail.google.com/'])
-
-    credentials = flow.run_console() if headless else flow.run_local_server()
-    save_credentials(name, credentials)
-
-
-@app.command()
-def get(
-        name: str = typer.Argument("default", help="Name for this credential."),
-        force_refresh: bool = typer.Option(False, help="Force-refresh token.")
-):
+def get_token(name: str, force_refresh: bool) -> Optional[str]:
 
     credentials_dict = load_credentials(name)
     if not {'client_id', 'client_secret', 'refresh_token'}.issubset(credentials_dict):
-        raise typer.Exit(code=1)
+        return None
 
     token = credentials_dict.get('token')
 
@@ -65,4 +48,39 @@ def get(
         save_credentials(name, credentials)
         token = credentials.token
 
+    return token
+
+@app.command()
+def authorize(
+        name: str = typer.Option("default", help="Name for this credential."),
+        client_secrets: Path = typer.Argument(..., help="Path to client secrets JSON file.",
+                                              exists=True, readable=True, dir_okay=False),
+        headless: bool = typer.Option(False, help="Run a console-based flow.")
+) -> None:
+
+    flow = InstalledAppFlow.from_client_secrets_file(client_secrets,
+                                                     scopes=['https://mail.google.com/'])
+
+    credentials = flow.run_console() if headless else flow.run_local_server()
+    save_credentials(name, credentials)
+
+
+@app.command()
+def get(
+        name: str = typer.Argument("default", help="Name for this credential."),
+        force_refresh: bool = typer.Option(False, help="Force-refresh token.")
+) -> None:
+    token = get_token(name, force_refresh)
+    if not token:
+        raise typer.Exit(code=1)
     print(token)
+
+
+@app.command()
+def check(
+        name: str = typer.Argument("default", help="Name for this credential."),
+        force_refresh: bool = typer.Option(False, help="Force-refresh token.")
+) -> None:
+    token = get_token(name, force_refresh)
+    if not token:
+        raise typer.Exit(code=1)
